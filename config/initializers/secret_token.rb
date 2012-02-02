f888fb2e3620bbb763b0a836256e1f537a0a668b
agranado <arthur.granado@21212.com>
2012-02-02 01:39:07 -0200

    Init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carnaval2012::Application.config.secret_token = '71e4ad1a405d44acc8ab2060464fa66073c5342435a38cf50a1538609cb571a3542d20a891157f0bbc47dc806494b2aa45e1d249dc7a8f7e349b66e339845298'
