986741f69cec9fee1a0e04d8f46c74300cd51994
agranado <arthur.granado@21212.com>
2012-02-02 03:47:22 -0200

    Include User and Authorization to store facebook user access_token, user_id, email and name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,25 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    render :text => auth_hash.inspect
+    @auth = Authorization.find_or_create(auth_hash)
+    if @auth
+      session[:user_id] = @auth.user_id
+      session[:access_token] = @auth.access_token
+    else
+      User.find(session[:user_id]).add_provider(auth_hash)
+    end
+
+    redirect_to root_path
   end
 
   def failure
+    render :text => "Sorry, but you didn't allow access to our app!"
   end
 
   def destroy
+    session[:user_id] = nil
+    session[:access_token] = nil
+    redirect_to root_path
   end
 
 end
