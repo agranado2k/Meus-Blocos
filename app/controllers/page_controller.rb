31378195fddf55a7053403c391f528dcadf9eff4
agranado <arthur.granado@21212.com>
2012-02-02 03:52:54 -0200

    Friend list is working

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -12,6 +12,10 @@ class PageController < ApplicationController
   end
 
   def friends
+    FbGraph.debug!
+    if session[:access_token]
+      @fb_user = FbGraph::User.me(session[:access_token]).fetch
+    end
   end
 
   def maps
